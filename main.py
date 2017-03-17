0e151459a3698557353bbcc1368c5f60772af39a
Frederic <frederic.branchaud-charron@usherbrooke.ca>
2017-03-17 10:37:04 -0400

    add sampler

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import tensorflow as tf
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--dataset_name', dest='dataset_name', default='facades', help='name of the dataset')
+parser.add_argument('--mean', dest='mean', type=int, default=64, help='# images used to train')
+parser.add_argument('--std', dest='std', type=int, default=32, help='# images used to train')
 parser.add_argument('--epoch', dest='epoch', type=int, default=200, help='# of epoch')
 parser.add_argument('--batch_size', dest='batch_size', type=int, default=50, help='# images in batch')
 parser.add_argument('--train_size', dest='train_size', type=int, default=1e8, help='# images used to train')
@@ -23,18 +25,25 @@ parser.add_argument('--beta1', dest='beta1', type=float, default=0.5, help='mome
 parser.add_argument('--flip', dest='flip', type=bool, default=True, help='if flip the images for data argumentation')
 parser.add_argument('--which_direction', dest='which_direction', default='AtoB', help='AtoB or BtoA')
 parser.add_argument('--phase', dest='phase', default='train', help='train, test')
-parser.add_argument('--save_epoch_freq', dest='save_epoch_freq', type=int, default=50, help='save a model every save_epoch_freq epochs (does not overwrite previously saved models)')
-parser.add_argument('--save_latest_freq', dest='save_latest_freq', type=int, default=5000, help='save the latest model every latest_freq sgd iterations (overwrites the previous latest model)')
-parser.add_argument('--print_freq', dest='print_freq', type=int, default=50, help='print the debug information every print_freq iterations')
-parser.add_argument('--continue_train', dest='continue_train', type=bool, default=False, help='if continue training, load the latest model: 1: true, 0: false')
-parser.add_argument('--serial_batches', dest='serial_batches', type=bool, default=False, help='f 1, takes images in order to make batches, otherwise takes them randomly')
-parser.add_argument('--serial_batch_iter', dest='serial_batch_iter', type=bool, default=True, help='iter into serial image list')
+parser.add_argument('--save_epoch_freq', dest='save_epoch_freq', type=int, default=50,
+                    help='save a model every save_epoch_freq epochs (does not overwrite previously saved models)')
+parser.add_argument('--save_latest_freq', dest='save_latest_freq', type=int, default=5000,
+                    help='save the latest model every latest_freq sgd iterations (overwrites the previous latest model)')
+parser.add_argument('--print_freq', dest='print_freq', type=int, default=50,
+                    help='print the debug information every print_freq iterations')
+parser.add_argument('--continue_train', dest='continue_train', type=bool, default=False,
+                    help='if continue training, load the latest model: 1: true, 0: false')
+parser.add_argument('--serial_batches', dest='serial_batches', type=bool, default=False,
+                    help='f 1, takes images in order to make batches, otherwise takes them randomly')
+parser.add_argument('--serial_batch_iter', dest='serial_batch_iter', type=bool, default=True,
+                    help='iter into serial image list')
 parser.add_argument('--checkpoint_dir', dest='checkpoint_dir', default='./checkpoint', help='models are saved here')
 parser.add_argument('--sample_dir', dest='sample_dir', default='./sample', help='sample are saved here')
 parser.add_argument('--test_dir', dest='test_dir', default='./test', help='test sample are saved here')
 parser.add_argument('--L1_lambda', dest='L1_lambda', type=float, default=100.0, help='weight on L1 term in objective')
 
 args = parser.parse_args()
+
 
 def main(_):
     if not os.path.exists(args.checkpoint_dir):
@@ -43,16 +52,24 @@ def main(_):
         os.makedirs(args.sample_dir)
     if not os.path.exists(args.test_dir):
         os.makedirs(args.test_dir)
+    cfg = dict({
+        'allow_soft_placement': False,
+        'log_device_placement': False
+    })
+    cfg['gpu_options'] = tf.GPUOptions(
+        per_process_gpu_memory_fraction=0)
+    cfg['allow_soft_placement'] = True
+    with tf.device('/cpu:0'):
+        with tf.Session(config=tf.ConfigProto(**cfg)) as sess:
+            model = pix2pix(sess, image_size=args.fine_size, batch_size=args.batch_size,
+                            output_size=args.fine_size, dataset_name=args.dataset_name,
+                            checkpoint_dir=args.checkpoint_dir, sample_dir=args.sample_dir,mean=args.mean,std=args.std)
 
-    with tf.Session() as sess:
-        model = pix2pix(sess, image_size=args.fine_size, batch_size=args.batch_size,
-                        output_size=args.fine_size, dataset_name=args.dataset_name,
-                        checkpoint_dir=args.checkpoint_dir, sample_dir=args.sample_dir)
+            if args.phase == 'train':
+                model.train(args)
+            else:
+                model.sample_model_for_test()
 
-        if args.phase == 'train':
-            model.train(args)
-        else:
-            model.test(args)
 
 if __name__ == '__main__':
     tf.app.run()
